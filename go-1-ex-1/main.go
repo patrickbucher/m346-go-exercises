1d680d38a6f686e4a507c402676d9bf6897a353f
Sirion Taiyasakda <Sirion_Taiyasakda@sluz.ch>
2024-11-22 14:48:48 +0100

    commit testing

--- go-1-ex-1/main.go
+++ go-1-ex-1/main.go
@@ -4,6 +4,8 @@ import "fmt"
 
 func main() {
 	// TODO: Declare and initialize the variables being used in the output!
+
+	fmt.Printf("Hello World by Siri")
 	fmt.Printf("Vor- und Nachname: %s %s\n", firstName, lastName)
 	fmt.Printf("Geburtsdatum: %d.%d.%d\n", dayOfBirth, monthOfBirth, yearOfBirth)
 	fmt.Printf("Anzahl Geschwister: %d\n", numberOfSiblings)
