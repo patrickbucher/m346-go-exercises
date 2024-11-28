92f66009795219b2283cdba1243cbf425b77a589
Eneas Infanger <eneas_infanger@sluz.ch>
2024-11-28 15:43:06 +0100

    exercise 1

--- go-1-ex-1/main.go
+++ go-1-ex-1/main.go
@@ -3,10 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	// TODO: Declare and initialize the variables being used in the output!
+	var firstName, lastName = "Eneas", "Infanger"
 	fmt.Printf("Vor- und Nachname: %s %s\n", firstName, lastName)
+	var dayOfBirth, monthOfBirth, yearOfBirth = 1, 3, 2008
 	fmt.Printf("Geburtsdatum: %d.%d.%d\n", dayOfBirth, monthOfBirth, yearOfBirth)
+	var numberOfSiblings = 2
 	fmt.Printf("Anzahl Geschwister: %d\n", numberOfSiblings)
+	var heightInMeters = 1.9
 	fmt.Printf("Grösse (in Metern): %.2f\n", heightInMeters)
+	var zodiacSign = '\u2653'
 	fmt.Printf("Sternzeichen: %c\n", zodiacSign)
 }
