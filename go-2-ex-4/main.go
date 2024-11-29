06a52afe1183b9dc8618d798b483e20863e0045a
Sirion Taiyasakda <Sirion_Taiyasakda@sluz.ch>
2024-11-29 18:47:52 +0100

    Aufgabe 2 ex 4 gelöst

--- go-2-ex-4/main.go
+++ go-2-ex-4/main.go
@@ -1,8 +1,56 @@
 package main
 
+import "fmt"
+
 func main() {
 	// TODO: declare a type for Student (with first and last name)
+	type Student struct {
+		Firstname string
+		Lastname  string
+	}
+
+	studentSiri := Student{
+		"Siri",
+		"On",
+	}
+
+	studentSirion := Student{
+		"Sirion",
+		"Taiyasakda",
+	}
+
+	studentTai := Student{
+		"Tai",
+		"Ya",
+	}
+
+	studentTaiya := Student{
+		"Taiya",
+		"Sakda",
+	}
+
+	var studenten = []Student{studentSiri, studentSirion}
+	var studenten2 = []Student{studentTai, studentTaiya}
+
 	// TODO: declare a type for Class (consisting of multiple students)
+	type Class struct {
+		Students []Student
+	}
+
+	var class1 = Class{
+		studenten,
+	}
+
+	var class2 = Class{
+		studenten2,
+	}
+
 	// TODO: declare a map of modules being attended by multiple classes
+	modules := map[int]Class{
+		117: class1,
+		118: class2,
+	}
+
 	// TODO: output everything using fmt.Println()
+	fmt.Println(modules)
 }
