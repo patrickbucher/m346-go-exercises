8a1152c7399f8eeabcbfc47a9ad8fdef5384229e
utigernils <utigernils@gmail.com>
2024-11-29 11:15:24 +0100

    Homeschooling Aufträge (Woche 2)

--- go-2-ex-3/main.go
+++ go-2-ex-3/main.go
@@ -3,14 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	// TODO: create a map called "modules"
+	modules := map[int]string{
+		104: "Introduction to Programming",
+		117: "Data Structures",
+		346: "Advanced Go Programming",
+	}
 
 	fmt.Println("Modul 104:", modules[104])
 	fmt.Println("Modul 117:", modules[117])
 	fmt.Println("Modul 346:", modules[346])
 
-	// TODO: delete one
-	// TODO: add one
-	// TODO: replace one
+	delete(modules, 117)
+
+	modules[200] = "Machine Learning"
+
+	modules[346] = "Concurrency in Go"
+
 	fmt.Println(modules)
 }
