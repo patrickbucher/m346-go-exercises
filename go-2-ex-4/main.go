04a81eb5bcfbd132ae85de21ab92ce2722b68916
Eneas Infanger <eneas_infanger@sluz.ch>
2024-12-05 16:07:46 +0100

    exercise 2-4

--- go-2-ex-4/main.go
+++ go-2-ex-4/main.go
@@ -1,8 +1,71 @@
 package main
 
+import "fmt"
+
+type Student struct {
+	FirstName string
+	LastName  string
+}
+
+type Class struct {
+	Students []Student
+}
+
 func main() {
-	// TODO: declare a type for Student (with first and last name)
-	// TODO: declare a type for Class (consisting of multiple students)
-	// TODO: declare a map of modules being attended by multiple classes
-	// TODO: output everything using fmt.Println()
+	modules := make(map[uint16][]Class, 0)
+
+	eneasStudent := Student{
+		FirstName: "Eneas",
+		LastName:  "Infanger",
+	}
+
+	timStudent := Student{
+		FirstName: "Tim",
+		LastName:  "Hüsler",
+	}
+
+	aaronStudent := Student{
+		FirstName: "Aaron",
+		LastName:  "Ettlin",
+	}
+
+	philStudent := Student{
+		FirstName: "Phil",
+		LastName:  "Hägeli",
+	}
+
+	arunStudent := Student{
+		FirstName: "Arun",
+		LastName:  "Aenishänslin",
+	}
+
+	class1 := Class{
+		Students: []Student{
+			eneasStudent,
+			timStudent,
+			aaronStudent,
+		},
+	}
+
+	class2 := Class{
+		Students: []Student{
+			aaronStudent,
+			philStudent,
+			arunStudent,
+		},
+	}
+
+	class3 := Class{
+		Students: []Student{
+			arunStudent,
+			eneasStudent,
+			timStudent,
+		},
+	}
+
+	modules[123] = []Class{class1, class3}
+	modules[346] = []Class{class1, class2, class3}
+	modules[114] = []Class{class2}
+
+	fmt.Println(modules)
 }
