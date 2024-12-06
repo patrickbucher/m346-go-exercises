f43a4ebde9ff9f18e0af994f4e576ab1619de83d
Sirion Taiyasakda <Sirion_Taiyasakda@sluz.ch>
2024-12-06 14:06:13 +0100

    Aufgabe 3 ex 3 überarbeitet

--- go-3-ex-3/main.go
+++ go-3-ex-3/main.go
@@ -11,17 +11,13 @@ func main() {
 	// TODO: Implement FizzBuzz using a for loop from Lower to Upper.
 	for i := Lower; i < Upper+1; i++ {
 		if i%3 == 0 && i%5 == 0 {
-			fmt.Println("\nFizzBuzz")
-			fmt.Println(i, "Number is devidable by 3 AND 5.")
+			fmt.Println("FizzBuzz")
 		} else if i%3 == 0 {
-			fmt.Println("\nFizz")
-			fmt.Println(i, "Number is devidable by 3.")
+			fmt.Println("Fizz")
 		} else if i%5 == 0 {
-			fmt.Println("\nBuzz")
-			fmt.Println(i, "Number is devidable by 5.")
+			fmt.Println("Buzz")
 		} else {
-			fmt.Println()
-			fmt.Println(i, "Number IS  NOT devidable by 3 OR 5 OR both.")
+			fmt.Println(i)
 		}
 	}
 }
