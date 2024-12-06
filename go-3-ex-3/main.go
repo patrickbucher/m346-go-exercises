03628bfa9a2d606a806f03f8b11de75fd9c73da8
yannickict <yblatty@edp.ch>
2024-12-06 17:40:45 +0100

    aufgaben 3

--- go-3-ex-3/main.go
+++ go-3-ex-3/main.go
@@ -7,4 +7,19 @@ const (
 
 func main() {
 	// TODO: Implement FizzBuzz using a for loop from Lower to Upper.
+	// buzz ist wenn es durch 5 teilbar ist
+	// fizz ist wenn es durch 3 teilbar ist
+	// fizzbuzz ist wenn es durch 3 und 5 teilbar ist
+	for n := Lower; n <= Upper; n++ {
+		switch {
+		case n%3 == 0 && n%5 == 0:
+			println("FizzBuzz")
+		case n%3 == 0:
+			println("Fizz")
+		case n%5 == 0:
+			println("Buzz")
+		default:
+			println(n)
+		}
+	}
 }
