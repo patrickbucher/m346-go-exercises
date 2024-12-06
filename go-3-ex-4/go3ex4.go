d1ff5f9952f9c0aa537034fea4b94e70712eb862
Fabian-Scheuch <fabian.scheuch@hotmail.com>
2024-12-06 14:03:40 +0100

    Add files via upload

---
+++ go-3-ex-4/go3ex4.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+const (
+	Diamonds = '\u25c6' // Karo
+	Spades   = '\u2660' // Pik
+	Clubs    = '\u2663' // Kreuz
+	Hearts   = '\u2665' // Herz
+
+	Six   = '\u2465'
+	Seven = '\u2466'
+	Eight = '\u2467'
+	Nine  = '\u2468'
+	Ten   = '\u2469'
+	Jack  = 'J'
+	Queen = 'Q'
+	King  = 'K'
+	Ace   = 'A'
+)
+
+func main() {
+	suits := []rune{Diamonds, Spades, Clubs, Hearts}
+	ranks := []rune{Six, Seven, Eight, Nine, Ten, Jack, Queen, King, Ace}
+
+	for _, value := range ranks {
+		for _, i := range suits {
+			fmt.Printf("%c%c\t", i, value)
+
+		}
+		fmt.Println()
+	}
+
+}
