34a3d892cd14ae9eb95c25552b1eb8c612a06dfe
Sirion Taiyasakda <Sirion_Taiyasakda@sluz.ch>
2024-12-06 14:01:40 +0100

    Aufgabe 3 ex 3 gelöst

--- go-3-ex-4/main.go
+++ go-3-ex-4/main.go
@@ -24,7 +24,14 @@ func main() {
 	ranks := []rune{Six, Seven, Eight, Nine, Ten, Jack, Queen, King, Ace}
 
 	// TODO: Loop over suits and ranks to output all combinations.
+	for _, rank := range ranks {
+		for _, suit := range suits {
+			fmt.Printf("%c%c ", suit, rank)
+		}
+
+		fmt.Println()
+	}
 
 	// TODO: delete this line afterwards
-	fmt.Println(suits, ranks)
+	//fmt.Println(suits, ranks)
 }
