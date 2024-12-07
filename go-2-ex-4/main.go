cf204e506495e8c1458fad6fd49718b9c74c6082
Benjamin Maier <Benjamin_Maier@sluz.ch>
2024-12-07 22:41:36 +0100

    solved excercises

--- go-2-ex-4/main.go
+++ go-2-ex-4/main.go
@@ -1,8 +1,66 @@
 package main
 
+import "fmt"
+
+type Student struct {
+	FirstName string
+	LastName  string
+}
+
+type Class struct {
+	Name     string
+	Students []Student
+}
+
+type Module struct {
+	ModuleNumber int
+	Classes      []Class
+}
+
 func main() {
-	// TODO: declare a type for Student (with first and last name)
-	// TODO: declare a type for Class (consisting of multiple students)
-	// TODO: declare a map of modules being attended by multiple classes
-	// TODO: output everything using fmt.Println()
+	studentsClassA := []Student{
+		{"Benjamin", "Luis"},
+		{"Julian", "Michael"},
+		{"Greggy", "Mike"},
+	}
+	studentsClassB := []Student{
+		{"Ioannis", "Kai"},
+		{"Ürner", "Davide"},
+		{"Saru", "Max"},
+	}
+
+	classA := Class{
+		Name:     "D23a",
+		Students: studentsClassA,
+	}
+	classB := Class{
+		Name:     "D23b",
+		Students: studentsClassB,
+	}
+
+	modules := []Module{
+		{
+			ModuleNumber: 346,
+			Classes:      []Class{classA, classB},
+		},
+		{
+			ModuleNumber: 104,
+			Classes:      []Class{classA},
+		},
+		{
+			ModuleNumber: 117,
+			Classes:      []Class{classB},
+		},
+	}
+
+	fmt.Println("Module:")
+	for _, module := range modules {
+		fmt.Printf("Modul %d beinhaltet:\n", module.ModuleNumber)
+		for _, class := range module.Classes {
+			fmt.Printf("  %s:\n", class.Name)
+			for _, student := range class.Students {
+				fmt.Printf("    - %s %s\n", student.FirstName, student.LastName)
+			}
+		}
+	}
 }
