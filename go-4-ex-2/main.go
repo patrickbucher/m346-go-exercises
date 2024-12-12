0b63820645ce9f488b3cf645b9d6c203c11cdfa8
Eneas Infanger <eneas_infanger@sluz.ch>
2024-12-12 16:24:58 +0100

    exercise 4-2

--- go-4-ex-2/main.go
+++ go-4-ex-2/main.go
@@ -1,7 +1,16 @@
 package main
 
-// TODO: implement the function computeHypotenuse using math.Pow and math.Sqrt
+import (
+	"fmt"
+	"math"
+)
+
+func computeHypotenuse(a float64, b float64) float64 {
+	return math.Sqrt(math.Pow(a, 2) + math.Pow(b, 2))
+}
 
 func main() {
-	// TODO: call the function computeHypotenuse
+	fmt.Println(computeHypotenuse(5.1, 7.65))               // 9.19
+	fmt.Println(computeHypotenuse(14.66, 11))               // 18.33
+	fmt.Println(computeHypotenuse(52151.52152, 7512.16901)) // 52689.79
 }
