5c9ffe64f47d805c98cfbe63adbffce4f60f8b0f
Artur Ferreira <artur.ferreira@ckw.ch>
2024-12-13 13:11:24 +0100

    go 4 ex 2 done

--- go-4-ex-2/main.go
+++ go-4-ex-2/main.go
@@ -1,7 +1,14 @@
 package main
 
+import "math"
+import "fmt"
 // TODO: implement the function computeHypotenuse using math.Pow and math.Sqrt
+func computeHypotenuse(a float64, b float64) float64{
+	c := math.Sqrt(math.Pow(a,2) + math.Pow(b,2))
 
+	return c
+}
 func main() {
 	// TODO: call the function computeHypotenuse
+	fmt.Print(computeHypotenuse(2,4))
 }
