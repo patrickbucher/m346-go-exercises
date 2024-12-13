8d8ae405155035a7a9ef42371737d722d6fa8013
Sirion Taiyasakda <Sirion_Taiyasakda@sluz.ch>
2024-12-13 13:17:54 +0100

    Aufgabe 4 ex 2 + Zusatzaufgabe gelöst

--- go-4-ex-2/main.go
+++ go-4-ex-2/main.go
@@ -1,7 +1,24 @@
 package main
 
+import (
+	"fmt"
+	"math"
+)
+
 // TODO: implement the function computeHypotenuse using math.Pow and math.Sqrt
+func computeHypotenuse(a float64, b float64) float64 {
+	c := math.Sqrt(math.Pow(2, a) + math.Pow(2, b))
+	return c
+}
+
+type ShortSides struct {
+	a float64
+	b float64
+}
 
 func main() {
 	// TODO: call the function computeHypotenuse
+	kathete := ShortSides{20, 41}
+	result := computeHypotenuse(kathete.a, kathete.b)
+	fmt.Println(result)
 }
