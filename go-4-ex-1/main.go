4c0c8dd876154e871cf966fa9669cde6deab0354
Sirion Taiyasakda <Sirion_Taiyasakda@sluz.ch>
2024-12-13 13:05:23 +0100

    Aufgabe 4 ex 1 gelöst

--- go-4-ex-1/main.go
+++ go-4-ex-1/main.go
@@ -1,7 +1,24 @@
 package main
 
+import "fmt"
+
 // TODO: implement the function computeGrade
+func computeGrade(gotPoints float32, maxPoints float32) float32 {
+	if gotPoints > maxPoints || gotPoints < 0 {
+		fmt.Printf("Deine Punktzahl %v ist ungültig. \n", gotPoints)
+	}
+
+	note := (gotPoints/maxPoints)*5 + 1
+
+	if note < 1 || note > 6 {
+		fmt.Print("Die Note ist ungültig: ")
+	}
+	return note
+}
 
 func main() {
 	// TODO: call the function computeGrade
+	fmt.Println(computeGrade(17.5, 28.0)) // 4.125
+	fmt.Println(computeGrade(30, 28.0))   // 4.125
+
 }
