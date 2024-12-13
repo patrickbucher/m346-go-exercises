d2a6a86b33d94819562d72a41a6e92fb9d34f2ee
Artur Ferreira <artur.ferreira@ckw.ch>
2024-12-13 13:04:58 +0100

    go 4 ex 1 done

--- go-4-ex-1/main.go
+++ go-4-ex-1/main.go
@@ -1,7 +1,12 @@
 package main
-
+import "fmt"
 // TODO: implement the function computeGrade
-
+func computeGrade(e float64, m float64) float64{
+	n := e/m * 5 + 1
+	return n
+}
 func main() {
 	// TODO: call the function computeGrade
+	grade := computeGrade(18,20)
+	fmt.Print(grade)
 }
