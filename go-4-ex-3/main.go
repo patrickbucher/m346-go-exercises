269e8dd80860ed9e13377787ec2c93ed1140e440
Eneas Infanger <eneas.infanger@loewenfels.ch>
2024-12-18 13:56:14 +0100

    exercise 4-3 zusatz

--- go-4-ex-3/main.go
+++ go-4-ex-3/main.go
@@ -5,8 +5,12 @@ import (
 	"math"
 )
 
+func computeDiscriminant(a float64, b float64, c float64) float64 {
+	return math.Pow(b, 2) - 4*a*c
+}
+
 func computeQuadraticFormula(a float64, b float64, c float64) []float64 {
-	var d = math.Pow(b, 2) - 4*a*c
+	var d = computeDiscriminant(a, b, c)
 
 	if d > 0 {
 		return []float64{
@@ -23,6 +27,12 @@ func computeQuadraticFormula(a float64, b float64, c float64) []float64 {
 }
 
 func main() {
+	fmt.Println("Diskriminante")
+	fmt.Println(computeDiscriminant(3, 4, 1)) // 4
+	fmt.Println(computeDiscriminant(2, 4, 2)) // 0
+	fmt.Println(computeDiscriminant(3, 4, 2)) // -8
+
+	fmt.Println("Mitternachtsformel")
 	fmt.Println(computeQuadraticFormula(3, 4, 1)) // [-0.3333333333333333 -1]
 	fmt.Println(computeQuadraticFormula(2, 4, 2)) // [-1]
 	fmt.Println(computeQuadraticFormula(3, 4, 2)) // []
