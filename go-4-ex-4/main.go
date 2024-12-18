23080c47ecf1aec078cbd1905629f1e4fa2823fb
Eneas Infanger <eneas.infanger@loewenfels.ch>
2024-12-18 15:14:03 +0100

    exercise 4-4 zusatz

--- go-4-ex-4/main.go
+++ go-4-ex-4/main.go
@@ -10,6 +10,17 @@ func convertFahrenheitToCelsius(f float64) float64 {
 	return (f - 32) * (5.0 / 9.0)
 }
 
+type Celsius float64
+type Fahrenheit float64
+
+func (c Celsius) ConvertToFahrenheit() Fahrenheit {
+	return Fahrenheit(convertCelsiusToFahrenheit(float64(c)))
+}
+
+func (f Fahrenheit) ConvertToCelsius() Celsius {
+	return Celsius(convertFahrenheitToCelsius(float64(f)))
+}
+
 func main() {
 	fmt.Println("Celsius to fahrenheit")
 	fmt.Println(convertCelsiusToFahrenheit(37.5)) // 99.5
@@ -20,4 +31,12 @@ func main() {
 	fmt.Println(convertFahrenheitToCelsius(99.5))  // 37.5
 	fmt.Println(convertFahrenheitToCelsius(29.3))  // -1.5
 	fmt.Println(convertFahrenheitToCelsius(123.8)) // 51
+
+	fmt.Println()
+
+	var cozy Celsius = 23.0
+	fmt.Println(cozy.ConvertToFahrenheit())
+
+	var cold Fahrenheit = 15.3
+	fmt.Println(cold.ConvertToCelsius())
 }
