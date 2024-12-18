6fd53e0f2df31554820141cecea8e8ae8a77e2db
Eneas Infanger <eneas.infanger@loewenfels.ch>
2024-12-18 13:51:45 +0100

    exercise 4-3

--- go-4-ex-3/main.go
+++ go-4-ex-3/main.go
@@ -1,7 +1,29 @@
 package main
 
-// TODO: implement the function computeQuadraticFormula
+import (
+	"fmt"
+	"math"
+)
+
+func computeQuadraticFormula(a float64, b float64, c float64) []float64 {
+	var d = math.Pow(b, 2) - 4*a*c
+
+	if d > 0 {
+		return []float64{
+			(-b + math.Sqrt(d)) / (2 * a),
+			(-b - math.Sqrt(d)) / (2 * a),
+		}
+	} else if d == 0 {
+		return []float64{
+			(-b + math.Sqrt(d)) / (2 * a),
+		}
+	} else {
+		return []float64{}
+	}
+}
 
 func main() {
-	// TODO: call the function computeQuadraticFormula
+	fmt.Println(computeQuadraticFormula(3, 4, 1)) // [-0.3333333333333333 -1]
+	fmt.Println(computeQuadraticFormula(2, 4, 2)) // [-1]
+	fmt.Println(computeQuadraticFormula(3, 4, 2)) // []
 }
