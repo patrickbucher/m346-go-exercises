174467964777a20b4d2bb6a4191199ed65811102
Lean Arnold <lean_arnold@sluz.ch>
2025-11-03 14:58:47 +0100

    A1 fertig

--- go-1-ex-1/main.go
+++ go-1-ex-1/main.go
@@ -1,6 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+var firstName string = "lean"
+var lastName string = "arnold"
+var dayOfBirth int = 13
+var monthOfBirth int = 05
+var yearOfBirth int = 2008
+var numberOfSiblings = 2
+var heightInMeters = 1.87
+var zodiacSign string = "taurus"
 
 func main() {
 	// TODO: Declare and initialize the variables being used in the output!
