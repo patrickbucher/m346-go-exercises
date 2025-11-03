c74b04689c239392440d7443986fb88e282a5ebc
ramon-csharp <ramon.zielke@outlook.com>
2025-11-03 15:21:35 +0100

    go 1 ex 1

--- go-1-ex-1/main.go
+++ go-1-ex-1/main.go
@@ -2,6 +2,15 @@ package main
 
 import "fmt"
 
+var firstName string = "Ramon"
+var lastName string = "Zielke"
+var dayOfBirth int = 30
+var monthOfBirth int = 1
+var yearOfBirth int = 2000
+var numberOfSiblings int = 3
+var heightInMeters float64 = 1.85
+var zodiacSign byte = 'W'
+
 func main() {
 	// TODO: Declare and initialize the variables being used in the output!
 	fmt.Printf("Vor- und Nachname: %s %s\n", firstName, lastName)
