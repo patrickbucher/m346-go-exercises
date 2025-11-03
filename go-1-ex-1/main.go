9e2263ed39bd6e9829fc1af96321fbb2f03b6bae
Alondra Prado <alondra.prado@apptiva.ch>
2025-11-03 14:52:24 +0100

    declared variables ex-1

--- go-1-ex-1/main.go
+++ go-1-ex-1/main.go
@@ -3,6 +3,14 @@ package main
 import "fmt"
 
 func main() {
+	firstName := "Alondra"
+	lastName := "Prado"
+	dayOfBirth := 23
+	monthOfBirth := 1
+	yearOfBirth := 2002
+	numberOfSiblings := 3
+	heightInMeters := 1.65
+	zodiacSign := 'W'
 	// TODO: Declare and initialize the variables being used in the output!
 	fmt.Printf("Vor- und Nachname: %s %s\n", firstName, lastName)
 	fmt.Printf("Geburtsdatum: %d.%d.%d\n", dayOfBirth, monthOfBirth, yearOfBirth)
