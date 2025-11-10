17498edba5ed03c57117c05f031291ddc16a1326
Pasha Stenkt <huhi94@outlook.com>
2025-11-10 14:38:18 +0100

    Update main.go

--- go-1-ex-1/main.go
+++ go-1-ex-1/main.go
@@ -4,6 +4,18 @@ import "fmt"
 
 func main() {
 	// TODO: Declare and initialize the variables being used in the output!
+
+	// Aufgabe
+	var firstName string = "Selina"
+	var lastName string = "Frey"
+	var dayOfBirth int = 17
+	var monthOfBirth int = 07
+	var yearOfBirth int = 2007
+	var numberOfSiblings int = 3
+	heightInMeters := 1.55
+	var zodiacSign = 'Cancer'
+	// Aufgabe
+
 	fmt.Printf("Vor- und Nachname: %s %s\n", firstName, lastName)
 	fmt.Printf("Geburtsdatum: %d.%d.%d\n", dayOfBirth, monthOfBirth, yearOfBirth)
 	fmt.Printf("Anzahl Geschwister: %d\n", numberOfSiblings)
