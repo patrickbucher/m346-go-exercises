1e73a5c44ae13b90965f629128984874e4280b98
Alondra Prado <alondra.prado@apptiva.ch>
2025-11-17 15:20:23 +0100

    finished go-2-ex-1

--- go-2-ex-1/main.go
+++ go-2-ex-1/main.go
@@ -4,12 +4,21 @@ import "fmt"
 
 type FullName struct {
 	// TODO: add fields
+	firstName string
+	lastName  string
 }
 
 // TODO: declare a structure for birth date
+type BirthDate struct {
+	dayOfBirth   byte
+	monthOfBirth byte
+	yearOfBirth  int16
+}
 
 type Profile struct {
 	// TODO: embed full name and birth date information
+	FullName
+	BirthDate
 	NumberOfSiblings byte
 	ZodiacSign       rune
 }
@@ -17,8 +26,17 @@ type Profile struct {
 func main() {
 	var me = Profile{
 		// TODO: set name and birth date information
-		NumberOfSiblings: 0,   // TODO: adjust
-		ZodiacSign:       ' ', // TODO: adjust
+		FullName: FullName{
+			firstName: "Alondra",
+			lastName:  "Prado",
+		},
+		BirthDate: BirthDate{
+			dayOfBirth:   23,
+			monthOfBirth: 1,
+			yearOfBirth:  2002,
+		},
+		NumberOfSiblings: 3,        // TODO: adjust
+		ZodiacSign:       '\u2652', // TODO: adjust
 	}
 	fmt.Println(me)
 
