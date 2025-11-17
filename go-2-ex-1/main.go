db921560f2a1197fde5f2c38697e0bb0469c41e4
Lean Arnold <lean_arnold@sluz.ch>
2025-11-17 15:14:56 +0100

    Aufgabe 1-3 Fertig

--- go-2-ex-1/main.go
+++ go-2-ex-1/main.go
@@ -3,26 +3,40 @@ package main
 import "fmt"
 
 type FullName struct {
-	// TODO: add fields
+	FirstName string
+	LastName  string
 }
 
-// TODO: declare a structure for birth date
+type BirthDate struct {
+	dayOfBirth   int
+	monthOfBirth int
+	yearOfBirth  int
+}
 
 type Profile struct {
-	// TODO: embed full name and birth date information
+	FullName
+	BirthDate
 	NumberOfSiblings byte
 	ZodiacSign       rune
 }
 
 func main() {
 	var me = Profile{
-		// TODO: set name and birth date information
-		NumberOfSiblings: 0,   // TODO: adjust
-		ZodiacSign:       ' ', // TODO: adjust
+		FullName: FullName{
+			FirstName: "Lean",
+			LastName:  "Arnold",
+		},
+		BirthDate: BirthDate{
+			dayOfBirth:   13,
+			monthOfBirth: 05,
+			yearOfBirth:  2008,
+		},
+		NumberOfSiblings: 2,
+		ZodiacSign:       '\u2649',
 	}
 	fmt.Println(me)
 
 	fmt.Println("Siblings Before:", me.NumberOfSiblings)
-	// TODO: imagine, you get a little brother or sister
+	me.NumberOfSiblings++
 	fmt.Println("Siblings After:", me.NumberOfSiblings)
 }
--- go-2-ex-3/main.go
+++ go-2-ex-3/main.go
@@ -4,13 +4,24 @@ import "fmt"
 
 func main() {
 	// TODO: create a map called "modules"
+	modules := map[int]string{
+		104: "Datenmodell implementieren",
+		117: "Informatik- und Netzinfrastruktur für ein kleines Unternehmen realisieren",
+		346: "Cloud Lösungen konzipieren und realisieren",
+	}
 
 	fmt.Println("Modul 104:", modules[104])
 	fmt.Println("Modul 117:", modules[117])
 	fmt.Println("Modul 346:", modules[346])
 
 	// TODO: delete one
+	delete(modules, 117)
+
 	// TODO: add one
+	modules[320] = "Objektorientiert Programmieren"
+
 	// TODO: replace one
+	modules[104] = "Datenmodell implementieren v4"
+
 	fmt.Println(modules)
 }
