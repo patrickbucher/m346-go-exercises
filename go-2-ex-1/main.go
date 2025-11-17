c0a694b7a46db01a8984b601e9a0fe8e21fe01e3
ramon-csharp <ramon.zielke@outlook.com>
2025-11-17 14:30:58 +0100

    go-2-ex-1

--- go-2-ex-1/main.go
+++ go-2-ex-1/main.go
@@ -3,26 +3,40 @@ package main
 import "fmt"
 
 type FullName struct {
-	// TODO: add fields
+	FirstName string
+	LastName  string	
 }
 
-// TODO: declare a structure for birth date
+type BirthDate struct {
+	Day   int
+	Month int
+	Year  int
+}
+
 
 type Profile struct {
-	// TODO: embed full name and birth date information
+	FullName        FullName
+	BirthDate      BirthDate
 	NumberOfSiblings byte
 	ZodiacSign       rune
 }
 
 func main() {
 	var me = Profile{
-		// TODO: set name and birth date information
-		NumberOfSiblings: 0,   // TODO: adjust
-		ZodiacSign:       ' ', // TODO: adjust
+		FullName: FullName{
+			FirstName: "Ramon",
+			LastName:  "Zielke",
+		},
+		BirthDate: BirthDate{
+			Day:   30, 
+			Month: 1,
+			Year:  2009,
+		},
+		NumberOfSiblings: 4,
+		ZodiacSign:       '\u2652',
 	}
 	fmt.Println(me)
 
 	fmt.Println("Siblings Before:", me.NumberOfSiblings)
-	// TODO: imagine, you get a little brother or sister
-	fmt.Println("Siblings After:", me.NumberOfSiblings)
+	fmt.Println("Siblings After:", me.NumberOfSiblings+1)
 }
