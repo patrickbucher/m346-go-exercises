d0bd26675d2840389a51d14c6d803c2de161c3df
ramon-csharp <ramon.zielke@outlook.com>
2025-11-17 14:47:24 +0100

    go-2-ex-3

--- go-2-ex-3/main.go
+++ go-2-ex-3/main.go
@@ -5,12 +5,18 @@ import "fmt"
 func main() {
 	// TODO: create a map called "modules"
 
+	modules := make(map[int]string)
+
+	modules[104] = "Datenmodell implementieren"
+	modules[117] = "ICT-Lösungen mit Blockchain Technologie umsetzen"
+	modules[346] = "Cloud Lösungen konzipieren und realisieren"
+
 	fmt.Println("Modul 104:", modules[104])
 	fmt.Println("Modul 117:", modules[117])
 	fmt.Println("Modul 346:", modules[346])
 
-	// TODO: delete one
-	// TODO: add one
-	// TODO: replace one
+	delete(modules, 346)
+	modules[320] = "Objektorientiert Programmieren"
+	modules[117] = "replaced module 117"
 	fmt.Println(modules)
 }
