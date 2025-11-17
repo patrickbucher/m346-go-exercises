4936524ab3a7992e0b83fc102a321698a7991156
LukasTraut <lukastraut@hotmail.com>
2025-11-17 15:24:07 +0100

    Nr.2 exe.3

--- go-2-ex-3/main.go
+++ go-2-ex-3/main.go
@@ -3,14 +3,22 @@ package main
 import "fmt"
 
 func main() {
-	// TODO: create a map called "modules"
+	modules := map[int]string{
+		104: "Modul 104:",
+		117: "Modul 117:",
+		346: "Modul 346:",
+}
 
 	fmt.Println("Modul 104:", modules[104])
 	fmt.Println("Modul 117:", modules[117])
 	fmt.Println("Modul 346:", modules[346])
 
-	// TODO: delete one
-	// TODO: add one
-	// TODO: replace one
+	delete(modules, 104) 
+
+	modules[114] = "Modul 114"
+
+	
+	modules[117] = "Modul 254" 
+
 	fmt.Println(modules)
 }
