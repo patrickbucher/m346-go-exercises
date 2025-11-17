9b0dd6a039abc6adf036b8e8988ac381d1d03dce
LukasTraut <lukastraut@hotmail.com>
2025-11-17 14:55:20 +0100

    Nr.2 exe1

--- go-2-ex-1/main.go
+++ go-2-ex-1/main.go
@@ -3,26 +3,41 @@ package main
 import "fmt"
 
 type FullName struct {
-	// TODO: add fields
+	FirstName string
+	LastName  string
 }
 
-// TODO: declare a structure for birth date
+type BirthDate struct {
+	Day   int
+	Month int
+	Year  int
+}
 
 type Profile struct {
-	// TODO: embed full name and birth date information
+	FullName
+	BirthDate
 	NumberOfSiblings byte
 	ZodiacSign       rune
 }
 
 func main() {
 	var me = Profile{
-		// TODO: set name and birth date information
-		NumberOfSiblings: 0,   // TODO: adjust
-		ZodiacSign:       ' ', // TODO: adjust
+		FullName: FullName{
+			FirstName: "Lukas",
+			LastName: "Traut",
+		},
+		BirthDate: BirthDate{
+			Day:   14,
+			Month: 11,
+			Year:  2008,
+		},
+		
+		NumberOfSiblings: 1,  
+		ZodiacSign:       '\u264F', 
 	}
 	fmt.Println(me)
 
 	fmt.Println("Siblings Before:", me.NumberOfSiblings)
-	// TODO: imagine, you get a little brother or sister
+	me.NumberOfSiblings++
 	fmt.Println("Siblings After:", me.NumberOfSiblings)
 }
