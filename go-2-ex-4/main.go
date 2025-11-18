d5e3085f331968eefc328b624c1cdaf40a64aab4
Alondra Prado <alondra.prado@apptiva.ch>
2025-11-18 15:40:27 +0100

    did go-2-ex-4

--- go-2-ex-4/main.go
+++ go-2-ex-4/main.go
@@ -1,8 +1,47 @@
 package main
 
+import "fmt"
+
+// TODO: declare a type for Student (with first and last name)
+type Student struct {
+	FirstName string
+	LastName  string
+}
+
+// TODO: declare a type for Class (consisting of multiple students)
+type Class struct {
+	Name     string
+	Students []Student
+}
+
 func main() {
-	// TODO: declare a type for Student (with first and last name)
-	// TODO: declare a type for Class (consisting of multiple students)
+	s1 := Student{FirstName: "Anna", LastName: "Müller"}
+	s2 := Student{FirstName: "Clara", LastName: "Weber"}
+
+	s3 := Student{FirstName: "David", LastName: "Meier"}
+	s4 := Student{FirstName: "Finn", LastName: "Schmidt"}
+
+	classInf23a := Class{
+		Name:     "Inf-23A",
+		Students: []Student{s1, s2},
+	}
+
+	classInf23b := Class{
+		Name:     "Inf-23B",
+		Students: []Student{s3, s4},
+	}
+
 	// TODO: declare a map of modules being attended by multiple classes
+	modules := map[int][]Class{
+		346: {classInf23a, classInf23b},
+		117: {classInf23a},
+		223: {classInf23b},
+	}
+
 	// TODO: output everything using fmt.Println()
+	fmt.Println("--- Klassenverwaltung ---")
+	fmt.Println("Klasse A:", classInf23a)
+	fmt.Println("Klasse B:", classInf23b)
+	fmt.Println("--- Modulbelegung ---")
+	fmt.Println(modules)
 }
