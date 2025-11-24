e4b303eb0ee7ee6c553c1101e63851850b9df39b
Pasha Stenkt <huhi94@outlook.com>
2025-11-24 08:56:21 +0000

    solved go-2-ex-1

--- go-2-ex-1/main.go
+++ go-2-ex-1/main.go
@@ -3,13 +3,22 @@ package main
 import "fmt"
 
 type FullName struct {
-	// TODO: add fields
+	// TODO: add fields$
+	FirstName string
+	LastName string
 }
 
 // TODO: declare a structure for birth date
-
+type BirthDate struct {
+    DayOfBirth   byte
+    MonthOfBirth byte
+    YearOfBirth  int16
+}
+ 
 type Profile struct {
 	// TODO: embed full name and birth date information
+	FullName
+	BirthDate
 	NumberOfSiblings byte
 	ZodiacSign       rune
 }
@@ -17,12 +26,24 @@ type Profile struct {
 func main() {
 	var me = Profile{
 		// TODO: set name and birth date information
-		NumberOfSiblings: 0,   // TODO: adjust
-		ZodiacSign:       ' ', // TODO: adjust
+		FullName: FullName{
+		FirstName:"Selina",
+		LastName:"Frey",
+		},
+		BirthDate: BirthDate{
+		DayOfBirth: 17,
+		MonthOfBirth:7,
+		YearOfBirth: 2007,
+		},
+		NumberOfSiblings: 3,   // TODO: adjust
+		ZodiacSign:       '\u264B',
+
 	}
 	fmt.Println(me)
 
 	fmt.Println("Siblings Before:", me.NumberOfSiblings)
 	// TODO: imagine, you get a little brother or sister
+	me.NumberOfSiblings = me.NumberOfSiblings + 1
+
 	fmt.Println("Siblings After:", me.NumberOfSiblings)
 }
